e40e01eb6a48da0b64940af8ecb9a3a45bb2ecdc
ju1115 <joo4123@naver.com>
2026-03-04 21:27:11 +0900

    28986 Погружение_в_Анимус 풀이

---
+++ BOJ/2603/04_28986_Погружение_в_Анимус.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 문제: BOJ 28986 Погружение_в_Анимус
+ * 난이도: BRONZE2
+ * 유형: 수학
+ */
+class Main_28986 {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        // System.setIn(new
+        // FileInputStream("BOJ/2603/04_28986_Погружение_в_Анимус.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // Code here
+        st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        long min = Math.max(1, k - n);
+        long max = (k - 1) / 2;
+        long answer = Math.max(0L, max - min + 1L);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
